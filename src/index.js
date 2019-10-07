2a039b7e1c3c33121d5afa699942acb40bd1e816
Zoran Obradović <zockyo@gmail.com>
2019-10-07 21:12:30 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { ShellApp as WebShellApp } from "@websh/web-shell-app";
+
+WebShellApp.manifest({
+  title:"ACE Editor",
+  icon: "/icon.svg",
+  api: {
+    file: {
+      formats: {
+        "text-file" : {
+          type: "text/plain text/*",
+          title: "Text File",
+          encoding: "text",
+          new: true,
+          open: true,
+          save: true
+        }
+      }
+    }
+  }
+})
+
+WebShellApp.on('file','open',({content,extension})=>{
+  const modes = {
+    js: "javascript",
+    sql: "sql"
+  };
+  const mode = modes[extension] || "text";
+  editor.session.setMode("ace/mode/"+mode);
+  
+  editor.setValue(content);
+})
+
+WebShellApp.on('file','save',()=>{
+  return editor.getValue();
+})
+
+WebShellApp.on('file','new',()=>{
+  editor.setValue("")
+})
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
